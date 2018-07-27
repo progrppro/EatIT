3a8c135daeabfc33a12f6a597773ebbd4c2e8765
Kaustubh Phatak <Kaustubh@localhost.localdomain>
2018-07-27 16:09:50 +0530

    Initial commit

---
+++ app/src/main/java/com/bhukkad/eatit/SignupActivity.java
@@ -0,0 +1,18 @@
+package com.bhukkad.eatit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SignupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+    }
+
+    public void event()
+    {
+
+    }
+}
